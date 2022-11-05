2203705afd82f2ad6f4d9810dcec115c9d670275
minhphan <58248684+theminh123@users.noreply.github.com>
2022-11-05 16:58:51 +0700

    .

---
+++ Midterm/Core/Inc/display7seg.h
@@ -0,0 +1,11 @@
+/*
+ * display7seg.h
+ *
+ *  Created on: Nov 5, 2022
+ *      Author: Panda
+ */
+
+#ifndef INC_DISPLAY7SEG_H_
+#define INC_DISPLAY7SEG_H_
+void display7SEG(int num);
+#endif /* INC_DISPLAY7SEG_H_ */
